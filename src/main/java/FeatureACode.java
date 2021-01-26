6a2fee4b87c8f0d8f4ae063923242577afc9fd8e
RaufVerdiyev <74197500+RaufVerdiyev@users.noreply.github.com>
2021-01-26 14:24:27 -0500

    more work

--- src/main/java/FeatureACode.java
+++ src/main/java/FeatureACode.java
@@ -1,6 +1,7 @@
 public class FeatureACode {
     public static void main(String[] args) {
-        System.out.println("Awsome work on FeatureA started");
+        System.out.println("Awesome work on FeatureA started");
+        System.out.println("B21 is learning more git with review");
     }
 
 
